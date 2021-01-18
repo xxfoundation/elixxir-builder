16ce57828904b0aea6099013bcfb3944d8992784
Richard T. Carback III <rick.carback@gmail.com>
2021-01-18 22:47:51 +0000

    Add initial build functionality

---
+++ builder/build.py
@@ -0,0 +1,112 @@
+"""
+building.py - utils for checking out and building
+"""
+
+import os
+import subprocess
+
+def get_dir(path):
+    return path.split(':')[1]
+
+def run(cmd):
+    print(" ".join(cmd))
+    result = subprocess.run(cmd, capture_output=True)
+    if result.returncode != 0:
+        print("Error: ", result)
+    return result
+
+def get_branch(repo):
+    cwd = os.getcwd()
+    os.chdir(get_dir(repo))
+    print("Path: ", os.getcwd())
+    res = run(['git', 'status'])
+    os.chdir(cwd)
+    for line in res.stdout.splitlines():
+        return line[len('On branch '):].decode('utf-8')
+
+def clone(repo):
+    if not repo:
+        raise("Cannot clone without repository!")
+
+    target_dir = get_dir(repo)
+    os.makedirs(target_dir, exist_ok=True)
+
+    cmd = ['git', 'clone', repo, target_dir]
+    run(cmd)
+
+def checkout(repo, branch):
+    if not repo or not branch:
+        raise("Cannot checkout without repo and branch!")
+
+    target_dir = get_dir(repo)
+    if not os.path.exists(target_dir):
+        clone(repo)
+
+    cwd = os.getcwd()
+    os.chdir(target_dir)
+    print("Path: ", os.getcwd())
+    cmds = [
+        ['git', 'checkout', '-B', branch],
+        ['git', 'branch', '--set-upstream-to=origin/{}'.format(branch), branch],
+        ['git', 'pull'],
+    ]
+    for cmd in cmds:
+        run(cmd)
+
+    os.chdir(cwd)
+
+
+def update(repo, dependencies):
+    if not repo:
+        raise("Cannot update without repo and dependencies")
+
+    if not dependencies:
+        print("No dependencies to update")
+
+    cwd = os.getcwd()
+    os.chdir(get_dir(repo))
+    for dep in dependencies:
+        cmd = ['go', 'get', '-u', dep]
+        res = run(cmd)
+        if res.returncode != 0:
+            os.chdir(cwd)
+            raise("ERROR!")
+    os.chdir(cwd)
+
+def build(repo):
+    if not repo:
+        raise("need repo to build!")
+    cwd = os.getcwd()
+    os.chdir(get_dir(repo))
+    res = run(['go', 'build', './...'])
+    os.chdir(cwd)
+    if res.returncode != 0:
+        raise("ERROR!")
+
+def test(repo):
+    if not repo:
+        raise("need repo to build!")
+    cwd = os.getcwd()
+    os.chdir(get_dir(repo))
+    res = run(['go', 'test', '-v', './...'])
+    os.chdir(cwd)
+    if res.returncode != 0:
+        raise("ERROR!")
+
+def status(repo):
+    if not repo:
+        raise("need repo to build!")
+    cwd = os.getcwd()
+    os.chdir(get_dir(repo))
+    run(['git', 'status'])
+    os.chdir(cwd)
+
+def push(repo):
+    if not repo:
+        raise("need repo to build!")
+    cwd = os.getcwd()
+    os.chdir(get_dir(repo))
+    res = run(['git', 'status'])
+    os.chdir(cwd)
+    if res.returncode != 0:
+        raise("ERROR!")
--- builder/main.py
+++ builder/main.py
@@ -4,12 +4,99 @@ main.py - main functionality for xxbuilder tool
 
 import click
 import sys
+import os
+import pathlib
 
-@click.command(help='XX Network Builder')
-@click.option('--output', '-o', type=click.File('wb'), default=sys.stdout,
-              help='Optional output file')
-def cli(output, seed, count):
-    print("Hello, world!\n", file=output)
+from builder import build
+
+proj_conf = {
+    'xxprimitives': {
+        'repo': 'git@gitlab.com:xx_network/primitives',
+        'dependencies': [],
+    },
+    'xxcrypto': {
+        'repo': 'git@gitlab.com:xx_network/crypto',
+        'dependencies': ['xxprimitives'],
+    },
+    'xxcomms': {
+        'repo': 'git@gitlab.com:xx_network/comms',
+        'dependencies': ['xxprimitives', 'xxcrypto'],
+    },
+    'primitives': {
+        'repo': 'git@gitlab.com:elixxir/primitives',
+        'dependencies': ['xxprimitives'],
+    },
+    'crypto': {
+        'repo': 'git@gitlab.com:elixxir/crypto',
+        'dependencies': ['xxprimitives', 'xxcrypto', 'primitives'],
+    },
+    'comms': {
+        'repo': 'git@gitlab.com:elixxir/comms',
+        'dependencies': ['xxprimitives', 'xxcrypto', 'xxcomms',
+                         'primitives', 'crypto'],
+    },
+    'server': { # Note -- leaving out GPU, ekv, and others for now!
+        'repo': 'git@gitlab.com:elixxir/server',
+        'dependencies': ['xxprimitives', 'xxcrypto', 'xxcomms',
+                         'primitives', 'crypto', 'comms'],
+    },
+    'gateway': {
+        'repo': 'git@gitlab.com:elixxir/gateway',
+        'dependencies': ['xxprimitives', 'xxcrypto', 'xxcomms',
+                         'primitives', 'crypto', 'comms'],
+    },
+    'client': {
+        'repo': 'git@gitlab.com:elixxir/client',
+        'dependencies': ['xxprimitives', 'xxcrypto', 'xxcomms',
+                         'primitives', 'crypto', 'comms'],
+    },
+    'udb': {
+        'repo': 'git@gitlab.com:elixxir/user-discovery-bot',
+        'dependencies': ['xxprimitives', 'xxcrypto', 'xxcomms',
+                         'primitives', 'crypto', 'comms', 'client'],
+    },
+}
+
+
+#@click.command(help='XX Network Builder')
+@click.group(chain=True)
+def cli():
+    os.chdir(os.path.join(pathlib.Path.home(), 'builder'))
+    print("Path: ", os.getcwd())
+
+#@cli.argument('pkg', help=('Any of {}'.format(','.join(proj_conf.keys()))))
+
+@cli.command('clone')
+def clone():
+    for k, v in proj_conf.items():
+        build.clone(v['repo'])
+
+@cli.command('checkout')
+@click.argument('branch')
+def checkout(branch):
+    for k, v in proj_conf.items():
+        build.checkout(v['repo'], branch)
+
+@cli.command('build')
+def buildcmd():
+    for k, v in proj_conf.items():
+        branch = str(build.get_branch(v['repo']))
+        dep_repos = [proj_conf[dep]['repo'] for dep in v['dependencies']]
+        deps = ['{}@{}'.format(repo, branch).replace(
+            'git@gitlab.com:', 'gitlab.com/') for repo in dep_repos]
+        build.update(v['repo'], deps)
+        build.build(v['repo'])
+
+@cli.command('test')
+def test():
+    for k, v in proj_conf.items():
+        branch = str(build.get_branch(v['repo']))
+        dep_repos = [proj_conf[dep]['repo'] for dep in v['dependencies']]
+        deps = ['{}@{}'.format(repo, branch).replace(
+            'git@gitlab.com:', "gitlab.com/") for repo in dep_repos]
+        build.update(v['repo'], deps)
+        build.test(v['repo'])
+
 
 if __name__ == '__main__':
     cli()
